c77ac259e5e86d39b736dca59204f273a0eef024
ezhang <edisonzhang714@gmail.com>
2024-04-11 13:15:32 -0400

    added data parsing

--- src/graph.jsx
+++ src/graph.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import LinePlot from './LinePlot'
+import chinaData from './assets/ChinaPop.json'
+import indData from './assets/IndPop.json'
+import usData from './assets/USPop.json'
 
-function Graph() {
+function Graph(date_range_min, date_range_max) {
+    const usPop = usData[1]
+    let graphData = []
+    let popData = usData[1].map(function(data){
+        if (data.date >= date_range_min && data.date <= date_range_max){
+            graphData.append(data.value)
+        }
+    })
     return (
         <LinePlot
-          data={[0, 1, 2, 3]}
+          data={graphData}
         />
     )
 }
