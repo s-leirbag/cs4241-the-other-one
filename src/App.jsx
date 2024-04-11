ff6774e8f957ba1253f39e4d068974659f5ba0aa
Gabriel Shiu <gabriel@shiu.us>
2024-04-11 12:52:14 -0400

    added d3, basic graph

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LinePlot from './LinePlot'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <LinePlot
+        data={[0, 1, 2, 3]}
+      />
     </>
   )
 }
