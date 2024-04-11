4e79f4a305d659ff73534dfb449501623be28ac1
TheLegacy-Coder <klaudiofushakako@gmail.com>
2024-04-11 13:25:42 -0400

    Attempted to display graph data

--- src/graph.jsx
+++ src/graph.jsx
@@ -3,18 +3,27 @@ import LinePlot from './LinePlot'
 import chinaData from './assets/ChinaPop.json'
 import indData from './assets/IndPop.json'
 import usData from './assets/USPop.json'
+import {useEffect, useState} from "react";
+
 
 function Graph(date_range_min, date_range_max) {
-    const usPop = usData[1]
-    let graphData = []
-    let popData = usData[1].map(function(data){
-        if (data.date >= date_range_min && data.date <= date_range_max){
-            graphData.append(data.value)
-        }
-    })
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        let graphData = []
+        usData[1].map(function(data){
+            if (data.date >= date_range_min && data.date <= date_range_max){
+                graphData.append(data.value)
+            }
+        })
+        setData(graphData);
+    }, []);
+
+    console.log(usData);
+    console.log(graphData)
     return (
         <LinePlot
-          data={graphData}
+          data={data}
         />
     )
 }
